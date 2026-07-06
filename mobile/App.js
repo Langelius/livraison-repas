9ce09eccf54382b3c66eaf1281fa579baaff0c70
langelius <53006719+Langelius@users.noreply.github.com>
2026-07-06 10:35:12 -0400

    feat: ajouter les écrans d'inscription et de connexion reliés à l'API (MEAL-01)

--- mobile/App.js
+++ mobile/App.js
@@ -2,6 +2,8 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import EcranConnexion from "./src/ecrans/EcranConnexion";
+import EcranInscription from "./src/ecrans/EcranInscription";
 import EcranAccueil from "./src/ecrans/EcranAccueil";
 
 const Pile = createNativeStackNavigator();
@@ -9,7 +11,17 @@ const Pile = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Pile.Navigator>
+      <Pile.Navigator initialRouteName="Connexion">
+        <Pile.Screen
+          name="Connexion"
+          component={EcranConnexion}
+          options={{ title: "Connexion" }}
+        />
+        <Pile.Screen
+          name="Inscription"
+          component={EcranInscription}
+          options={{ title: "Créer un compte" }}
+        />
         <Pile.Screen
           name="Accueil"
           component={EcranAccueil}
