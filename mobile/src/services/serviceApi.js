db8451bc09ed744e002cccd19d18c3274cb86d0e
langelius <53006719+Langelius@users.noreply.github.com>
2026-07-06 10:34:08 -0400

    feat: ajouter le client HTTP authentifié et le stockage de session (MEAL-02)

--- mobile/src/services/serviceApi.js
+++ mobile/src/services/serviceApi.js
@@ -1,14 +1,9 @@
 // Service d'appel à l'API REST
-import axios from "axios";
+import clientHttp from "./clientHttp";
 import Plat from "../modeles/plat";
 
-// 10.0.2.2 = localhost de la machine vu depuis l'émulateur Android
-const urlBase = "http://10.0.2.2:3000/api";
-
 export async function obtenirPlats(restaurantId) {
-  const reponse = await axios.get(
-    `${urlBase}/restaurants/${restaurantId}/plats`
-  );
+  const reponse = await clientHttp.get(`/restaurants/${restaurantId}/plats`);
 
   // Boucle explicite pour construire la liste
   const plats = [];
